2459b71860451373cd61562f41e5eef2a758c944
spriya96 <30333676+spriya96@users.noreply.github.com>
2017-08-15 18:31:02 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("%d",c);
+scanf("%c",&c);
+if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+printf("the character id alphapet");
+else
+printf("the character is not alphabet")
+
+return 0;
+
+}
